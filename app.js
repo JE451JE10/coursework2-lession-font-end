041440af38981b9bb0a45ffce284f7df86b3f7c1
James Epstein <je451@live.mdx.ac.uk>
2022-01-18 15:30:22 +0000

    update files

--- app.js
+++ app.js
@@ -35,4 +35,42 @@ app.get('/collection/:collectionName', (req, res) => {
     })
 })
 
+// retrieve an object by mongodb ID
+const ObjectID = require('mongodb').id; app.get('/collection/:collectionName/:id', (req, res, next) => {
+    req.collection.findOne(
+        { id: new id(req.params.id) }, (e, result) => {
+            if (e) return next(e)
+            res.send(result)
+        })
+})
+
+// add an object
+app.post('/collection/:collectionName', (req, res, next) => {
+    req.collection.insert(req.body, (e, results) => {
+        if (e) return next(e)
+        res.send(results.ops)
+    })
+})
+
+// update an object by ID
+app.put('/collection/:collectionName/:id', (req, res, next) => {
+    req.collection.update(
+        { id: new id(req.params.id) },
+        { $set: req.body }, { safe: true, multi: false }, (e, result) => {
+            if (e) return next(e)
+            res.send((result.result.n === 1) ?
+                { msg: 'success' } : { msg: 'error' })
+        })
+})
+
+// delete an object by ID
+app.delete('/collection/:collectionName/:id', (req, res, next) => {
+    req.collection.deleteOne(
+        { id: id(req.params.id) }, (e, result) => {
+            if (e) return next(e)
+            res.send((result.result.n === 1) ?
+                { msg: 'success' } :
+                { msg: 'error' })
+        })
+})
 app.listen(3000)
